3edf615ef3541e5c404cac351b5c79a7286c1aa6
vitorraiaa <vitortraia@gmail.com>
2023-10-24 13:40:18 -0300

    Protótipo de movimentação do cenário + player

--- jogo.py
+++ jogo.py
@@ -1,6 +1,5 @@
 import pygame
 from constantes import *
-import random
 
 def inicializa():
     pygame.init()
@@ -8,24 +7,78 @@ def inicializa():
     pygame.display.set_caption('Corre!')
     return window
 
-def recebe_eventos():
-    game = True
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game = False
-    return game
 
-def desenha(window):
-    window.fill((0, 0, 0))
-    pygame.display.update()
+
+class Player(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        self.surface_player = pygame.Surface((50, 25))
+        self.surface_player.fill((0, 255, 0))
+        self.rect_player = self.surface_player.get_rect(x=largura_tela * 0.1, y=altura_tela * 0.5)
+
+    def movimenta_player(self):
+        tecla_apertada = pygame.key.get_pressed()
+
+        if tecla_apertada[pygame.K_UP]:
+            self.rect_player.y -= 5
+        if tecla_apertada[pygame.K_DOWN]:
+            self.rect_player.y += 5
+
+    def desenha_player(self, window):
+        window.blit(self.surface_player, self.rect_player)
+
+
+
+
+class Background(pygame.sprite.Sprite):
+    
+    def __init__(self, imagem):
+        self.surface_background = pygame.image.load(imagem)
+        self.rect_background_esquerda = self.surface_background.get_rect(topleft=(0, 0))
+        self.rect_background_direita = self.surface_background.get_rect(topleft=(largura_tela, 0))
+
+    def movimenta_background(self):
+        self.rect_background_esquerda.x -= velocidade_tela
+        self.rect_background_direita.x -= velocidade_tela
+
+        if self.rect_background_esquerda.x <= -largura_tela:
+            self.rect_background_esquerda.x = largura_tela
+        if self.rect_background_direita.x <= -largura_tela:
+            self.rect_background_direita.x = largura_tela
+
+    def desenha_background(self, window):
+        window.blit(self.surface_background, self.rect_background_esquerda)
+        window.blit(self.surface_background, self.rect_background_direita)
+
+
+
+
 
 def game_loop(window):
-    # TODO: receber assets como argumento e repassar para desenha
-    while recebe_eventos():
-        desenha(window)
+    player = Player() 
+    background_imagem = Background('Background_pygame_project.webp')  
+
+    clock = pygame.time.Clock()  
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+        player.movimenta_player()  
+        background_imagem.movimenta_background()  
+
+        background_imagem.desenha_background(window)  
+        player.desenha_player(window) 
+
+        pygame.display.update()  
+
+        clock.tick(60) 
+
+
 
 
 if __name__ == '__main__':
-    # TODO: receber assets aqui e repassar para game_loop
     w = inicializa()
     game_loop(w)
